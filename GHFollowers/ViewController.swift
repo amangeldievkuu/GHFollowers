06c1f58312a6c7af327e8b4eb8d7a74105ddf590
amangeldievkuu <amangeldievkuu@gmail.com>
2024-12-23 23:48:06 +0800

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Adam Amangeldiev on 2024/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
