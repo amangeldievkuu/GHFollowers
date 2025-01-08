3632150c82c61abe1824d60a7ffc61f0c00730cb
amangeldievkuu <amangeldievkuu@gmail.com>
2025-01-09 00:07:31 +0800

    optimizations

--- GHFollowers/Extensions/UIView+Ext.swift
+++ GHFollowers/Extensions/UIView+Ext.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 extension UIView {
+    
     func addSubviews(_ views: UIView...) {
         views.forEach(addSubview)
+    }
+    
+    func pinToEdges(of superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor),
+        ])
     }
 }
--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -41,6 +41,8 @@ class FavoritesListVC: GFDataLoadingVC {
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.removeExcessCells()
+        
         tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
     }
     
@@ -90,16 +92,20 @@ extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
         navigationController?.pushViewController(destVC, animated: true)
     }
     
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
         
         let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
+        
         PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
             guard let self = self else { return }
             
-            guard let error = error else { return }
+            guard let error = error else {
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
             self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue)
         }
     }
